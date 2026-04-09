dfc4331699001bd326223d91e9331359f110ed75
Davide Frison <davide.frison@zextras.com>
2026-04-09 10:25:01 +0200

    chore: split setup and start of envoy (custom entrypoint and setup)

---
+++ docker/docs-editor-sidecar/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "[sidecar] Waiting for consul agent..."
+until consul members >/dev/null 2>&1; do sleep 1; done
+echo "[sidecar] Consul agent ready."
+if [ -n "${SETUP_SCRIPT}" ]; then
+  (${SETUP_SCRIPT} || true)
+fi
+consul services register /etc/zextras/service-discover/carbonio-docs-editor.hcl
+
+# See registry.dev.zextras.com/dev/carbonio-sidecar base entrypoint - just start sidecar in this case
+/usr/local/bin/entrypoint.sh do_start
